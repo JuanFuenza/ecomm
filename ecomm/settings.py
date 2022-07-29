bb91ba13452ec49706adcaeedaf33198fbbf3595
Juan Fuenzalida <juan.fuenza97@gmail.com>
2022-07-29 11:17:02 -0300

    cart/checkout fixes

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from secret import key
+from .secret import key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'
 
 SESSION_COOKIE_AGE = 86400
 CART_SESSION_ID = 'cart'
